33e0822b986f4d0cb5105eec50850e79a0b1aa66
taeseokyi <taeseokyi@gmail.com>
2020-04-03 13:58:15 +0900

    사이드바 추가

--- tsyi-nlp-app.py
+++ tsyi-nlp-app.py
@@ -4,7 +4,15 @@ def main():
   """ A simple NLP App """
   st.title("TITLE")
   st.subheader("Streamlit is cool.")
-
+  st.sidebar.title("Navigation")
+  goto_menu = st.sidebar.radio("Go to",
+    ("Home", "기능1", "기능2"))
+  if goto_menu == 'Home':
+      st.title("TITLE Home")
+      st.subheader("Streamlit is cool.")
+  elif goto_menu == "기능1":
+      st.title("기능1")
+      st.subheader("Streamlit is cool.")
 
 if __name__ == '__main__':
   main()
